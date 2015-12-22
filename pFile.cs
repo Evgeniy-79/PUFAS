dd05285e824090835d22e4a6b8ab404c496e06e0
Evgeniy-79 <zheka_m94@mail.ru>
2015-12-22 20:06:48 +1000

    1

---
+++ pFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PUFAS
+{
+    public class pFile
+    {
+        private string name;
+        private string size;
+        private DateTime date;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+    }
+}
